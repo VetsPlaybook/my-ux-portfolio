fb0b4f8a2a7cca94c5f57e9506f441b6b2034397
joseph morris <vetsplaybook@gmail.com>
2022-08-06 10:57:03 -0400

    trying again

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
 import Homepage from "../components/pages/homepage/Homepage";
-import NavBar from "../components/uiComponents/navigationBar/NavBar";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
       <Homepage />
     </>
   );
