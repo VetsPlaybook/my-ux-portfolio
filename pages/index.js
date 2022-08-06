6072939db1f165133581b4f2cdf72b8a970a5970
joseph morris <vetsplaybook@gmail.com>
2022-08-06 11:24:41 -0400

    adding navbar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Homepage from "../components/pages/homepage/Homepage";
+import NavBar from "@components/ui/navigationBar/Navbar";
 
 export default function Home() {
   return (
     <>
+      <NavBar />
       <Homepage />
     </>
   );
