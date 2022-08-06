0372814e649c4a7503b73f905bb09c90e06c0860
joseph morris <vetsplaybook@gmail.com>
2022-08-06 09:58:12 -0400

    first commit

---
+++ components/pages/homepage/Homepage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Homepage.module.css";
+
+function Homepage() {
+  return (
+    <>
+      <div className={classes.background}>
+        <div className={classes.verticalLine} />
+        <div className={classes.introTitle}>
+          <h3>Hello, I am</h3>
+          <h1>Joseph Morris</h1>
+          <h4>Professional UI/UX & Web Developer</h4>
+          <p>Lets work together to create the right user experience for your customers and stakeholders</p>
+        </div>
+        <img src="images/selfPortrait.png" className={classes.image} />
+      </div>
+    </>
+  );
+}
+
+export default Homepage;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Homepage from "../components/pages/homepage/Homepage";
+import NavBar from "../components/uiComponents/navigationBar/NavBar";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+    <>
+      <NavBar />
+      <Homepage />
+    </>
+  );
 }
