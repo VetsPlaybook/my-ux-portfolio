a30b4ac0f380aef72f95db44435babe0e84e8c89
joseph morris <vetsplaybook@gmail.com>
2022-08-08 22:03:44 -0400

    update to mobile view

--- components/pages/homepage/Homepage.js
+++ components/pages/homepage/Homepage.js
@@ -11,8 +11,10 @@ function Homepage() {
           <h1>Joseph Morris</h1>
           <h4>Professional UI/UX & Web Developer</h4>
           <p>Lets work together to create the right user experience for your customers and stakeholders</p>
+          <div className={classes.buttonContainer}>
           <button className={classes.button1}>View my work</button>
           <button className={classes.button2}>Work with me!</button>
+          </div>
         </div>
         <img src="images/selfPortrait.png" className={classes.image} />
         <img src="images/selfPortrait2.png" className={classes.image2} />
