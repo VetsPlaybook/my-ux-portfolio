2f64861c8df2890a8632ecacc8a4a36ecce31b8d
joseph morris <vetsplaybook@gmail.com>
2022-08-08 21:50:46 -0400

    day 1 completed

--- components/pages/homepage/Homepage.js
+++ components/pages/homepage/Homepage.js
@@ -11,9 +11,12 @@ function Homepage() {
           <h1>Joseph Morris</h1>
           <h4>Professional UI/UX & Web Developer</h4>
           <p>Lets work together to create the right user experience for your customers and stakeholders</p>
+          <button className={classes.button1}>View my work</button>
+          <button className={classes.button2}>Work with me!</button>
         </div>
         <img src="images/selfPortrait.png" className={classes.image} />
         <img src="images/selfPortrait2.png" className={classes.image2} />
+        
       </div>
     </>
   );
---
+++ components/ui/button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./Button.module.scss"
+
+function Button() {
+  return (
+    <button className={classes.button}>Button</button>
+  )
+}
+
+export default Button
