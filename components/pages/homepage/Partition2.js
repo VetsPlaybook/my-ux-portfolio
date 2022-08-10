e6377c7561a3e3ff5c49a3b15a133f9ecc291acb
joseph morris <vetsplaybook@gmail.com>
2022-08-09 22:56:59 -0400

    day 2 complete

---
+++ components/pages/homepage/Partition2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./Partition2.module.scss";
+
+function Partition2() {
+  return (
+    <div className={classes.container}>
+        <img src="images/partition2.png" className={classes.image}/>
+    </div>
+  )
+}
+
+export default Partition2
---
+++ components/pages/homepage/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from "./Projects.module.scss";
+import { motion } from "framer-motion";
+
+function Projects() {
+  return (
+    <div className={classes.container}/>
+  )
+}
+
+export default Projects
