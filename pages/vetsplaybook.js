08d04711ea0b33f29f2851946c0e73156a84646c
joseph morris <vetsplaybook@gmail.com>
2022-08-15 01:34:18 -0400

    added animation to vpb case study

--- pages/vetsplaybook.js
+++ pages/vetsplaybook.js
@@ -2,13 +2,23 @@ import Introduction from "@components/pages/vets playbook/Introduction";
 import IntroductionVideo from "@components/pages/vets playbook/IntroductionVideo";
 import Title from "@components/pages/vets playbook/Title";
 import React from "react";
+import { motion } from "framer-motion";
 
 function vetsplaybook() {
   return (
     <>
       <Title />
-      <Introduction />
-      <IntroductionVideo />
+      <motion.div
+        initial={{ opacity: "0%", y: 50 }}
+        animate={{
+          opacity: "100%",
+          y: 0,
+          transition: { duration: 0.8, delay: 0.8 },
+        }}
+      >
+        <Introduction />
+        <IntroductionVideo />
+      </motion.div>
     </>
   );
 }
